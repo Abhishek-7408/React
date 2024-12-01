f75f87a0ed86aacc8fa489632ebfedfcfc63ca95
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2024-12-01 10:50:40 +0530

    01-12-2024

---
+++ 01basicprojectvite/01viteReact/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <h1>Chai aur react with vite | Abhishek Kushwaha</h1>
+
+  )
+ 
+}
+
+export default App
---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+  
+<h1>Chai Aur React | Abhishek Kushwaha</h1>
+
+  );
+}
+
+export default App;
